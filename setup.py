cf137f4bca8089e652695168df343b18f1256440
Jin Xu <jinuxstyle@hotmail.com>
2016-06-20 11:04:05 +0800

    Improve long description

--- setup.py
+++ setup.py
@@ -6,6 +6,32 @@ from setuptools import setup
 import clipon
 from clipon.defines import CLIPON_VERSION
 
+long_desc="""Clipon is a open source project for managing your
+clipboard history in a simple and efficient way.
+
+It's useful if you want to take notes more efficiently when reading
+documents or web pages. It trackes whatever you copied so that you
+don't have to switch to another app to do paste whenever you copied
+something useful. The paste operation is kind of a distraction when
+you want to focus on reading. With Clipon, you just copy things when
+reading, and do paste in batch after reading.
+
+It also helps remember what you have copied and you might later want to
+revisit some of them.
+
+It's simple to use, start the daemon
+
+    $ clipon start
+
+Then you can view the clipboard history with following command
+
+    $ clipon list
+
+To see more subcommands, use
+
+    $ clipon -h | --help
+"""
+
 setup(
     name='clipon',
     version=CLIPON_VERSION,
@@ -13,7 +39,7 @@ setup(
     author='Jin Xu',
     author_email='jinuxstyle@hotmail.com',
     description='Clipon - a lightweight clipboard manager',
-    long_description="Clipon is a open source project for managing your clipboard history in a simple and efficient way.",
+    long_description=long_desc,
     url="https://github.com/jinuxstyle/clipon",
     packages=['clipon'],
     platforms='linux',
